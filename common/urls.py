7c2e7a99f1d52c3bf0e6b41a3bc949755fc53d6f
Your Name <you@example.com>
2023-02-15 15:49:41 -0500

    add link to footer

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = "common"
 urlpatterns = [
     path("", home),
-    path("api_doc", api_doc),
+    path("api-doc/", api_doc),
     path("home/", home, name="home"),
 ]
