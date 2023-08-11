61ca5f86293f9ab8024ccf5a6b5896d58a8d0c7d
Your Name <you@example.com>
2023-08-11 12:53:27 -0400

    opml import: log user action

--- journal/importers/opml.py
+++ journal/importers/opml.py
@@ -1,21 +1,12 @@
-import logging
-import os
-import re
-from datetime import datetime
-
 import django_rq
 import listparser
-import openpyxl
-import pytz
-from django.conf import settings
-from django.core.files import uploadedfile
-from markdownify import markdownify as md
+from auditlog.context import set_actor
+from loguru import logger
 from user_messages import api as msg
 
 from catalog.common import *
 from catalog.common.downloaders import *
 from catalog.sites.rss import RSS
-from common.utils import GenerateDateUUIDMediaFilePath
 from journal.models import *
 
 
@@ -36,32 +27,33 @@ class OPMLImporter:
         return True
 
     def import_from_file_task(self, feeds):
-        print(f"{self.user} import opml start")
+        logger.info(f"{self.user} import opml start")
         skip = 0
         collection = None
-        if self.mode == 1:
-            collection = Collection.objects.create(
-                owner=self.user, title=f"{self.user.display_name}的播客订阅列表"
-            )
-        for feed in feeds:
-            print(f"{self.user} import {feed.url}")
-            res = RSS(feed.url).get_resource_ready()
-            if not res:
-                print(f"{self.user} feed error {feed.url}")
-                continue
-            item = res.item
-            if self.mode == 0:
-                mark = Mark(self.user, item)
-                if mark.shelfmember:
-                    print(f"{self.user} marked, skip {feed.url}")
-                    skip += 1
-                else:
-                    mark.update(
-                        ShelfType.PROGRESS, None, None, visibility=self.visibility
-                    )
-            elif self.mode == 1 and collection:
-                collection.append_item(item)
-        print(f"{self.user} import opml end")
+        with set_actor(self.user):
+            if self.mode == 1:
+                collection = Collection.objects.create(
+                    owner=self.user, title=f"{self.user.display_name}的播客订阅列表"
+                )
+            for feed in feeds:
+                logger.info(f"{self.user} import {feed.url}")
+                res = RSS(feed.url).get_resource_ready()
+                if not res:
+                    logger.error(f"{self.user} feed error {feed.url}")
+                    continue
+                item = res.item
+                if self.mode == 0:
+                    mark = Mark(self.user, item)
+                    if mark.shelfmember:
+                        logger.info(f"{self.user} marked, skip {feed.url}")
+                        skip += 1
+                    else:
+                        mark.update(
+                            ShelfType.PROGRESS, None, None, visibility=self.visibility
+                        )
+                elif self.mode == 1 and collection:
+                    collection.append_item(item)
+        logger.info(f"{self.user} import opml end")
         msg.success(
             self.user,
             f"OPML导入完成，共处理{len(feeds)}篇，已存在{skip}篇。",
