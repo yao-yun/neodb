c664f2ce4e75817794fdb8fc28ef3d14b32259ed
Your Name <you@example.com>
2024-01-14 13:14:59 -0500

    increase client id size limit as catodon/firefish may generate lengthy ids

--- mastodon/models.py
+++ mastodon/models.py
@@ -4,12 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class MastodonApplication(models.Model):
-    domain_name = models.CharField(_("site domain name"), max_length=100, unique=True)
-    api_domain = models.CharField(_("domain for api call"), max_length=100, blank=True)
-    server_version = models.CharField(_("type and verion"), max_length=100, blank=True)
-    app_id = models.CharField(_("in-site app id"), max_length=100)
-    client_id = models.CharField(_("client id"), max_length=100)
-    client_secret = models.CharField(_("client secret"), max_length=100)
+    domain_name = models.CharField(_("site domain name"), max_length=200, unique=True)
+    api_domain = models.CharField(_("domain for api call"), max_length=200, blank=True)
+    server_version = models.CharField(_("type and verion"), max_length=200, blank=True)
+    app_id = models.CharField(_("in-site app id"), max_length=200)
+    client_id = models.CharField(_("client id"), max_length=200)
+    client_secret = models.CharField(_("client secret"), max_length=200)
     vapid_key = models.CharField(_("vapid key"), max_length=200, null=True, blank=True)
     star_mode = models.PositiveIntegerField(
         _("0: custom emoji; 1: unicode moon; 2: text"), blank=False, default=0
@@ -22,9 +22,6 @@ class MastodonApplication(models.Model):
     is_proxy = models.BooleanField(default=False, blank=True)
     proxy_to = models.CharField(max_length=100, blank=True, default="")
 
-    # website
-    # name
-    # redirect_uris
     def __str__(self):
         return self.domain_name
 
