738cfc20aded41901dca6cf2e0f0f2f5625b643d
Your Name <you@example.com>
2024-07-14 01:34:17 -0400

    tweak language detection

--- catalog/common/tests.py
+++ catalog/common/tests.py
@@ -1,5 +1,15 @@
-from django.test import TestCase as DjangoTestCase
+from django.test import TestCase
+
+from common.models import detect_language
 
 
-class TestCase(DjangoTestCase):
+class CommonTestCase(TestCase):
     databases = "__all__"
+
+    def test_detect_lang(self):
+        lang = detect_language("The Witcher 3: Wild Hunt")
+        self.assertEqual(lang, "en")
+        lang = detect_language("巫师3：狂猎")
+        self.assertEqual(lang, "zh-cn")
+        lang = detect_language("巫师3：狂猎 The Witcher 3: Wild Hunt")
+        self.assertEqual(lang, "zh-cn")
