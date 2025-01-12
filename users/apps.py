56f11fbf9d20f804ec963cd1361999b506f86712
mein Name <ich@example.com>
2025-01-12 12:09:19 -0500

    fix missing user api registration

--- users/apps.py
+++ users/apps.py
@@ -5,5 +5,7 @@ class UsersConfig(AppConfig):
     name = "users"
 
     def ready(self):
+        from . import api  # noqa
+
         # register cron jobs
         from users.jobs import MastodonUserSync  # noqa
